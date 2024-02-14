09cef075e886e650001b2df62c1959f57aacb4d3
Balram Kusharam <balramkushram120@gmail.com>
2024-02-15 00:29:01 +0530

    Second Commit

--- Backened/app.js
+++ Backened/app.js
@@ -1,14 +1,15 @@
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const seedDB = require("./seed");
 const cors = require("cors");
-const quotesRoutes = require('./apis/quoteRoutes.js');
+const quotesRoutes = require("./apis/quoteRoutes.js");
+const dotenv = require('dotenv').config();
 
+let url =
+  "mongodb+srv://balram:123@cluster0.2gwzyjt.mongodb.net/QuotesAppretryWrites=true&w=majority";
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Quotes")
+  .connect(url)
   .then(() => {
     console.log("DB Connected");
   })
@@ -16,28 +17,26 @@ mongoose
     console.log("Error in DB", err);
   });
 
-  // ⬇️ app.use(cors());     // this middleware give permission to access any origin 
+// ⬇️ app.use(cors());     // this middleware give permission to access any origin
 
-  app.use(
-    cors(
-    {
-      origin: ["http://localhost:5173"],
-    }
-  ));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
-  app.use(express.json());   // Body Parser for Json
-  app.use(express.urlencoded({ extended: true }));  // Body Parser for form
+app.use(express.json()); // Body Parser for Json
+app.use(express.urlencoded({ extended: true })); // Body Parser for form
 
 app.get("/", (req, res) => {
   res.send("Welcome to Backend");
 });
 
-
 // seedDB();
 
 app.use(quotesRoutes);
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`server is connected at port: ${PORT}`);
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is connected at port: ${process.env.PORT}`);
 });
